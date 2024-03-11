7cf9877fdab92d104e790527f7f1823b3ccc087e
Zehra Karaca <106537527+zehrakaraca@users.noreply.github.com>
2024-03-11 20:25:52 +0300

    Add files via upload

---
+++ lesson.dart
@@ -0,0 +1,12 @@
+class Lesson {
+    String lessonName;
+    int time;
+
+    Lesson(this.lessonName, this.time);
+
+    void printLessonInfo(){
+        print('Lesson Name: $lessonName Time: $time minute');
+    }
+
+  printCourseInfo() {}
+}
